db290d2b4c2a2b8d8eae2223439f390fb5af86ee
Pedro Cristo <pcristo35@gmail.com>
2023-06-05 13:34:22 +0100

    Remove unused scripts and CSS for Recaptcha

--- src/assets/js/my_script.js
+++ src/assets/js/my_script.js
@@ -68,26 +68,3 @@ function ParalaxReady(){
 
 }
 
-// RECAPTCHA RESPONSIVE
-$(function () {
-    function rescaleCaptcha() {
-        var width = $('.g-recaptcha').parent().width();
-        var scale;
-        if (width < 302) {
-            scale = width / 302;
-        } else {
-            scale = 1.0;
-        }
-        $('.g-recaptcha').css('transform', 'scale(' + scale + ')');
-        $('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
-        $('.g-recaptcha').css('transform-origin', '0 0');
-        $('.g-recaptcha').css('-webkit-transform-origin', '0 0');
-    }
-    rescaleCaptcha();
-    $(window).resize(function () {
-        rescaleCaptcha();
-    });
-});
-export default ParalaxReady;
-
-
