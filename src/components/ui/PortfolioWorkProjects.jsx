e5238e0250696c23911a6156df4aa47037cc045d
Pedro Cristo <pcristo35@gmail.com>
2023-07-03 16:54:51 +0100

    Update portfolio card logic to open project URLs in a new tab if the project in not  ID is 9, otherwise open them in the same tab

--- src/components/ui/PortfolioWorkProjects.jsx
+++ src/components/ui/PortfolioWorkProjects.jsx
@@ -1,22 +1,34 @@
 import PropTypes from "prop-types";
 
-
-function PortfolioWorkProjectsEn({ titleWork, classWork, workProjects, buttonWork, langPt }) {
+function PortfolioWorkProjectsEn({
+  titleWork,
+  classWork,
+  workProjects,
+  buttonWork,
+  langPt,
+}) {
   return (
     <section id="work-section" className="work parallax-2">
       <div className="max-width">
         <h2 className={classWork}>{titleWork}</h2>
         <div className="carousel owl-carousel">
           {workProjects.map((project) => (
-           <div className={`card ${project.id === 8 ? "card-logo" : null}`} key={project.id}>
+            <div
+              className={`card ${project.id === 8 ? "card-logo" : null}`}
+              key={project.id}
+            >
               <a
-                href={project.projectUrl}
-                target="_blank"
+                href={project.id === 9 ? "/" : project.projectUrl}
+                target={project.id === 9 ? "_self" : "_blank"}
                 rel="noopener noreferrer"
               >
                 <div className="box">
                   <img src={project.projectImg} alt={project.projectName} />
-                  <h2 className="text">{langPt === true && project.id === 1 ? `Restaurante Nortada` : `${project.projectName}`}</h2>
+                  <h2 className="text">
+                    {langPt === true && project.id === 1
+                      ? `Restaurante Nortada`
+                      : `${project.projectName}`}
+                  </h2>
                   <div className="box-icons">
                     {project.projectTechIcons.map((icon) => (
                       <i key={icon.id} className={icon.iconClass}></i>
@@ -32,9 +44,17 @@ function PortfolioWorkProjectsEn({ titleWork, classWork, workProjects, buttonWor
                         />
                       ))}
                   </div>
-                  <h5 className="btn-portfolio">{buttonWork}</h5>
+                  {project.id === 9 ?  <h5 className="btn-portfolio">Live Version</h5> : (
+                    <h5 className="btn-portfolio">{buttonWork}</h5>
+                  )}
                 </div>
-                {project.id === 8 ? <img src="images/portfolio/logos/zarco_logo.png" alt="LOGO IMG" className="logo-box" /> : null}
+                {project.id === 8 ? (
+                  <img
+                    src="images/portfolio/logos/zarco_logo.png"
+                    alt="LOGO IMG"
+                    className="logo-box"
+                  />
+                ) : null}
               </a>
             </div>
           ))}
@@ -50,6 +70,6 @@ PortfolioWorkProjectsEn.propTypes = {
   workProjects: PropTypes.array.isRequired,
   buttonWork: PropTypes.string.isRequired,
   langPt: PropTypes.bool,
-}
+};
 
 export default PortfolioWorkProjectsEn;
