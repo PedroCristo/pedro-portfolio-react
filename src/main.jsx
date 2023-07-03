e287034339a2a7227e11e3a78a5d88d6b7a39209
Pedro Cristo <pcristo35@gmail.com>
2023-07-03 16:29:06 +0100

    Set scroll smoth for the home page

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,9 @@ import BackToTop from './components/ui/BackToTop.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div className='main'>
     <App />
     <BackToTop />
+    </div>
   </React.StrictMode>,
 )
